8538a4c70f822614035e437701964ac85b12adf4
yuriprym <samuel.young.103@gmail.com>
2015-10-12 23:03:00 -0400

    hopefully this should allow the split up pokeball into individual characters

--- HundredYearOldCell/cell.c
+++ HundredYearOldCell/cell.c
@@ -1,5 +1,26 @@
-/* my first project with c 
+/* my first project with c
 
 
 */
 #include<cell.c>
+#include<stdio.h>
+
+main()
+{
+    int x;
+    for (x=0; x<10;x++)
+    {
+        if (x%2==0){
+            printf("Hello World");
+        }
+        if (x%5==0)
+            printf("bar")
+        else{
+            printf("foo");
+        }
+
+        char* itema= "pokeball"
+    }
+    getchar();
+
+}
