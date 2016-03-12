db31f510d4e4f237098ac2050be7f23a6122e9d9
yuriprym <samuel.young.103@gmail.com>
2016-03-12 18:39:48 +0000

    i modifed the code in DNA so that is would imporve the usage of DNA

--- HundredYearOldCell/HundredYearOldCell/cDNA.c
+++ HundredYearOldCell/HundredYearOldCell/cDNA.c
@@ -3,10 +3,24 @@
 #include <stdio.h>
 #include <organicmole.h>
 //creating the DNA backbone in the bits to code;
+int main {int DNA char* reversing}
+{
+
+
+
+
+}
+int DNA{long int DNAa,long int DNAt,long int DNAg,long int DNAc, long int DNAs,long int DNAe,}
+{
+ if  {DNAa xor DNAt}
+   
+
+}
+
 
 
 // hard coding the DNA;
-char revesring (char* hardcode, char c, char a, char t, char g, char s, char m)
+char* revesring (char* hardcode, char c, char a, char t, char g, char s, char m)
 {
     
 }
