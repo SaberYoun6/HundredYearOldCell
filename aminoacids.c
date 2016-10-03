a432829beff96ed95c50db701d99ff251bbdfb2a
Young <youngse7@compile.vcu.edu>
2016-10-03 15:31:36 -0400

    this are the orignal files that i had in the at the end of the cells

---
+++ aminoacids.c
@@ -0,0 +1,13 @@
+#include <organicmole.h>
+#include <cDNA.h>
+#include <intationalRNAStuc.h>
+#include <cRNA.h>
+// this is that it will begin by producing proteins that are and use them up
+aminoAcid(){
+        metethoine = RNAa+RNAu+RNAg;
+        if(RNAa + RNAa + RNAa || RNAa + RNAu+ RNAu || RNAa + RNAu +RNAc)
+            {
+                isolecine;
+            }
+
+}
---
+++ cellsturture.c
@@ -0,0 +1,10 @@
+// celluar base
+#include <cDNA.h>
+#include <cRNA.h>
+#include <aminoAcids.h>
+#include <organicmole.h>
+#include <intiatioalRNAStuc.h>
+
+
+
+/* call all functional groups that exist within side of the cellular sturturces that i'm going to be creating with the end results of proteins */
