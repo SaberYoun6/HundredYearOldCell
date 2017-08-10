a245889c8125f8b494f93091830d606ef4fe3085
yuriprym <samuel.young.103@gmail.com>
2017-08-10 00:02:42 -0600

    add quarks types in which I will put them all together

---
+++ protons.c
@@ -0,0 +1,6 @@
+#include<quarks.h>
+#include<stdio.h>
+#include"protons.h"
+#include"math.h"
+//this will be the next stop once I am done with the quarks calls
+
---
+++ quarks.c
@@ -0,0 +1,24 @@
+#include"quarks.h"
+#include<stdio.h>
+#include<math.h>
+
+
+//the quark will be defined then it will be used to generate the math in the matrix in which i will be using
+//the matrix will be a 2d to use them time 
+float **quark(float spin, float rotation, float axis,float energy){
+
+}
+float ** upQuark(float spin, float rotation,float energy){
+}
+float ** downQuarks(float spin, float rotation, float energy){
+}
+float ** strangeQuarks(){
+}
+float ** charmQuarks(){
+}
+float ** topQuarks(){
+}
+float ** botQuarks(){
+}
+
+
