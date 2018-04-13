89233a545675a1b4c71edf532cbc2a276e796875
yuriprym <samuel.young.103@gmail.com>
2018-04-13 10:39:00 -0400

    add a few files and then went and add a comment to the eletron

--- electrons.c
+++ electrons.c
@@ -1,9 +1,10 @@
-// This contain the electron math for understanding how the from
+// This contain the electron the way to find out how any electron has the potential to do work
 //
 //
-
+// this is the math to determine the amount of energy that is stored in one electron
 float * electron_velocity(float energy, float mass){
-   return *(energy /mass) 
+   return *(energy /mass);
+
 
  
 
