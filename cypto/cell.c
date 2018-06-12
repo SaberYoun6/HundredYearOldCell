2cdabfab497bb75439ec117e4e524057ae957ee5
yuriprym <samuel.young.103@gmail.com>
2018-06-12 11:42:47 -0400

    I'm trying to figure out what is going on

---
+++ cypto/cell.c
@@ -0,0 +1,8 @@
+/* my first project with c*/
+// so 
+#include <cDNA.h>
+#include <cRNA.h>
+#include <aminoAcids.h>
+#include <organicmole.h>
+#include <intiatioalRNAStuc.h>
+#include <cellsturture.h>
